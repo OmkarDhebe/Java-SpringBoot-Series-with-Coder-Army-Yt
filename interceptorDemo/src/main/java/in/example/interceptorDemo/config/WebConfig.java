b459b700c28bdc04db2e500aff20341dc982a86c
OmkarDhebe <omkardhebe0000@gmail.com>
2026-07-20 19:24:07 +0530

    Interceptor Demo

---
+++ interceptorDemo/src/main/java/in/example/interceptorDemo/config/WebConfig.java
@@ -0,0 +1,39 @@
+package in.example.interceptorDemo.config;
+
+import in.example.interceptorDemo.interceptor.AuthenticationInterceptor;
+import in.example.interceptorDemo.interceptor.AuthorizationInterceptor;
+import in.example.interceptorDemo.interceptor.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public LoggingInterceptor loggingInterceptor;
+    public AuthenticationInterceptor authenticationInterceptor;
+    public AuthorizationInterceptor authorizationInterceptor;
+
+    public WebConfig(LoggingInterceptor loggingInterceptor,
+           AuthenticationInterceptor authenticationInterceptor,
+           AuthorizationInterceptor authorizationInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+        this.authenticationInterceptor = authenticationInterceptor;
+        this.authorizationInterceptor = authorizationInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/auth/login", "api/public/**")
+                .order(1);
+
+        registry.addInterceptor(loggingInterceptor)
+                        .order(3);
+
+        registry.addInterceptor(authorizationInterceptor)
+                .order(2);
+    }
+}
+
